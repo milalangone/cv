5bb820d5b315962ca8a8e207c370b1d24a1341e9
milalangone <mlangone@itba.edu.ar>
2024-07-03 12:34:33 -0300

    Add header for different sections

--- scripts.js
+++ scripts.js
@@ -1,13 +1,46 @@
 // Muestra un mensaje de bienvenida en la consola
-console.log("¡Bienvenido a mi CV en línea!");
+console.log("Bienvenido a mi CV en línea");
 
 // Ejemplo de interactividad: mostrar una alerta al hacer clic en un botón
 document.addEventListener("DOMContentLoaded", function() {
-    const button = document.createElement("button");
-    button.textContent = "Haz clic aquí para más información";
-    document.getElementById("boton-container").appendChild(button);
 
-    button.addEventListener("click", function() {
-        alert("¡Gracias por visitar mi CV!");
+    const Button_Home = document.getElementById("Button_Home");
+    const Button_Projects = document.getElementById("Button_Projects");
+    const Button_Experience = document.getElementById("Button_Experience");
+    const Button_Education = document.getElementById("Button_Education");
+    const Button_Contact = document.getElementById("Button_Contact");
+
+    const homeSection = document.getElementById("home");
+    const projectsSection = document.getElementById("projects");
+    const experienceSection = document.getElementById("experience");
+    const educationSection = document.getElementById("education");
+    const contactSection = document.getElementById("contact");
+
+    Button_Home.addEventListener("click", function() {
+        toggleSection(homeSection);
     });
+
+    Button_Projects.addEventListener("click", function() {
+        toggleSection(projectsSection);
+    });
+
+    Button_Experience.addEventListener("click", function() {
+        toggleSection(experienceSection);
+    });
+    Button_Education.addEventListener("click", function() {
+        toggleSection(educationSection);
+    });
+
+    Button_Contact.addEventListener("click", function() {
+        toggleSection(contactSection);
+    });
+
+    function toggleSection(section) {
+        // Oculta todas las secciones
+        const allSections = document.querySelectorAll(".cv-section");
+        allSections.forEach(sec => sec.classList.remove("show"));
+
+        // Muestra la sección específica
+        section.classList.add("show");
+    }
 });
